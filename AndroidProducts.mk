25906256ac36e0ce309b73b1cdcd61dd698b5d0a
ELbee <85691313+woodyfi@users.noreply.github.com>
2024-02-20 15:54:59 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # Inherit Redmi Note 7 device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xperience_lavender.mk
+    $(LOCAL_DIR)/aosp_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    xperience_lavender-eng \
-    xperience_lavender-userdebug \
-    xperience_lavender-user
+    aosp_lavender-eng \
+    aosp_lavender-userdebug \
+    aosp_lavender-user
